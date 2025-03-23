3ef85c66bf091397c683a510ff0e902c7b3b99e5
Malcolm <mstone@rollins.edu>
2025-03-23 16:45:43 -0400

    Set up static file serving and configure EJS as the view engine

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
