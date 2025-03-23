5a5d818d1da59bd1a493f7de1aadda663c65cf23
Malcolm <mstone@rollins.edu>
2025-03-23 16:47:42 -0400

    Change server port to 3001 and add home route for rendering

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
