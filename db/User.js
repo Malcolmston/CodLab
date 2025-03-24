e5ea0873f5d9d9f8d32262fc8a5f118460d445f9
Malcolm <mstone@rollins.edu>
2025-03-24 12:12:36 -0400

    Implement User model with validation and password hashing

---
+++ db/User.js
@@ -0,0 +1,74 @@
+(require('dotenv')).config()
+
+const sequelize = require('./connect');
+const { DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+
+const SALT = process.env.SALT || 10;
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    firstname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            len: [3, 255]
+        }
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            len: [1, 255]
+        }
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            notEmpty: true,
+            len: [3, 255]
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            is: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+            // This regex checks for at least one lowercase letter, one uppercase letter, one digit, and a minimum length of 8 characters
+        }
+    }
+},{
+    paranoid: true,
+    sequelize,
+    modelName: 'User',
+    hooks: {
+        beforeCreate: (user) => {
+            user.password = bcrypt.hashSync(user.password, SALT);
+        },
+
+        beforeUpdate: (user) => {
+            // check if password is being updated
+            if (user.changed('password')) {
+                user.password = bcrypt.hashSync(user.password, SALT);
+            }
+        }
+    }
+});
