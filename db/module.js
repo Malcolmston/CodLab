686b0ea674d231999fc3408727d3630503a7b315
Malcolm <mstone@rollins.edu>
2025-03-24 13:38:34 -0400

    Add module export for database models and sync with the database

---
+++ db/module.js
@@ -0,0 +1,18 @@
+const File = require('./Files');
+const User = require('./User');
+const Project = require('./Project');
+const Role = require('./UserFile');
+const sequelize = require('./connect');
+
+// Sync all models with the database
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log('Database & tables created!');
+    });
+
+module.exports = {
+    File,
+    User,
+    Project,
+    Role
+};
