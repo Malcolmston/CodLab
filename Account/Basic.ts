eec1c70bd4ac4b62b6532863cf5c09daac8212f1
Malcolm <mstone@rollins.edu>
2025-03-25 09:12:27 -0400

    Add Basic class extending Account with login and sign-up methods

---
+++ Account/Basic.ts
@@ -0,0 +1,69 @@
+import Account, { Type } from "./Account";
+import * as db from "../db";
+
+
+class Basic extends Account {
+
+    constructor(firstname: string, lastname: string, email: string, username: string, password: string) {
+        super(firstname, lastname, email, username, password, Type.Basic);
+    }
+
+    public static async Basic (id: number) {
+        let user = await db.User.findOne({
+            where: {
+                id
+            }
+        });
+
+        if( !user ) return null;
+
+        let basic = new Basic(
+            user.firstname,
+            user.lastname,
+            user.email,
+            user.username,
+            user.password,
+        );
+
+        basic.id = user.id;
+        return basic;
+    }
+
+    async login(username: string, password: string): Promise<boolean> {
+        let user = await db.User.findOne({
+            where: {
+                username
+            }
+        });
+
+        if( !user ) return false;
+
+        return !(await this.isDeleted()) && await this.doseExist() && Account.compare(password, user.password);
+    }
+
+
+    async signUp(): Promise<boolean> {
+      if( (await this.isDeleted()) || (await this.doseExist()) ) return false;
+      let transaction = await db.sequelize.transaction();
+
+      try {
+        await db.User.create({
+            firstname: this.firstname,
+            lastname: this.lastname,
+            email: this.email,
+            username: this.username,
+            password: this.password,
+            type: Type.Basic
+        }, { transaction });
+
+        await transaction.commit();
+        return true;
+    } catch (error) {
+        await transaction.rollback();
+        console.error("Error during sign up:", error);
+        return false;
+      }
+        
+    }
+    
+}
